07123e5e8538f2fb46f1925182fb45e6bdc61575
qiangjun <zhangqj616@gmail.com>
2020-09-22 09:39:22 +0100

    refactor: wrapped arguments into objects

--- src/game_object.py
+++ src/game_object.py
@@ -1,13 +1,29 @@
 import pygame
 
 
-class GameObject:
-    def __init__(self, x, y, speed_x, speed_y, width, length):
+class Point:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.speed_x = speed_x
-        self.speed_y = speed_y
-        self.rect = pygame.Rect(x, y, width, length)
+
+
+class Speed(Point):
+    pass
+
+
+class Shape:
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+
+
+class GameObject:
+    def __init__(self, point, speed, shape):
+        self.x = point.x
+        self.y = point.y
+        self.speed_x = speed.x
+        self.speed_y = speed.y
+        self.rect = pygame.Rect(self.x, self.y, shape.width, shape.length)
         self.color = (27, 235, 60)
 
     def set_color(self, color):
--- src/pong_application.py
+++ src/pong_application.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from src.ball import Ball
 from src.game_manager import Manager
+from src.game_object import Speed, Point, Shape
 from src.game_window import GameWindow
 from src.player import Player
 
@@ -12,15 +13,31 @@ pygame.display.set_caption('Ping Pong')
 
 screen_width = 1280
 screen_height = 960
-bg_color = pygame.Color('grey12')
-light_grey = (27, 235, 60)
+bg_color = pygame.Color('grey20')
+light_grey = pygame.Color('green')
 game_window = GameWindow(screen_width, screen_height)
 
-ball = Ball(game_window.get_width() // 2 - 15, game_window.get_height() // 2 - 15, 7, 7, 30, 30)
-ball.set_color((200, 10, 10))
-player = Player(game_window.get_width() - 20, game_window.get_height() // 2 - 70, 0, 0, 10, 500)
-player.set_color((20, 210, 210))
-opponent = Player(10, screen_height // 2 - 70, 0, 7, 10, 140)
+ball_point = Point(game_window.get_width() // 2 - 15, game_window.get_height() // 2 - 15)
+ball_speed = Speed(7, 7)           # speed at x and y direction
+ball_shape = Shape(30, 30)
+ball = Ball(ball_point, ball_speed, ball_shape)
+ball_color = pygame.Color('blue')
+ball.set_color(ball_color)
+
+player_point = Point(game_window.get_width() - 20, game_window.get_height() // 2 - 70)
+player_speed = Speed(0, 0)         # speed at x and y direction
+player_shape = Shape(10, 140)
+player = Player(player_point, player_speed, player_shape)
+player_color = pygame.Color('white')
+player.set_color(player_color)
+
+opponent_point = Point(10, screen_height // 2 - 70)
+opponent_speed = Speed(0, 7)         # speed at x and y direction
+opponent_shape = Shape(10, 140)
+opponent = Player(opponent_point, opponent_speed, opponent_shape)
+opponent_color = pygame.Color('green')
+opponent.set_color(opponent_color)
+
 game_manager = Manager(ball, player, opponent)
 
 
